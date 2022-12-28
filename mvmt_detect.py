a1a8b8e98d38439bc20899dcd6db326e320e5e50
MilindShekhawat <milindsinghshekhawat139129144@gmail.com>
2022-12-28 21:50:15 +0530

    Added VideoWriter to the code.
    VideoWriter is a functionality in OpenCV that enables us to create/write a video using tracked frames. We use VideoWriter to create an output video of tracked humans.

--- mvmt_detect.py
+++ mvmt_detect.py
@@ -12,7 +12,7 @@ hogcv.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 # %%
 def detect(frame):
     # Detect where to ahd How to create a Bounding Box
-    bounding_box_cordinates, weights =  hogcv.detectMultiScale(frame, winStride = (4, 4), padding = (8, 8), scale = 1.03)
+    bounding_box_cordinates, weights =  hogcv.detectMultiScale(frame, winStride = (8, 8), padding = (8, 8), scale = 1.05)
     # Render the bounding Box
     for x,y,w,h in bounding_box_cordinates:
         cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0), 2)
@@ -23,23 +23,45 @@ def detect(frame):
     return frame
 
 # %%
-# Read the source
+# Read Frame Data for videowriter
+def framedata(path):
+    video = cv2.VideoCapture(path, apiPreference=cv2.CAP_MSMF)
+    _, frame = video.read()
+    frame = imutils.resize(frame , width=min(600,frame.shape[1]))
+    #height and width
+    (h, w) = frame.shape[:2]
+    #frames per second
+    fps = video.get(cv2.CAP_PROP_FPS)
+    
+    return h,w,fps
+
+# %%
+# Read the source video
 def videoinput(path):
     # Frame by Frame Input Reading
     video = cv2.VideoCapture(path, apiPreference=cv2.CAP_MSMF)
-
+    h,w,fps = framedata(path)
+    # VideoWriter enables writing frames to output
+    result = cv2.VideoWriter('outpy.mp4',cv2.VideoWriter_fourcc(*'mp4v'), fps, (w,h))
+    
     while True:
         # Reading Frames
-        ret, frame =  video.read()
-        # Resizing Frames for better performance
-        frame = imutils.resize(frame , width=min(600,frame.shape[1]))
-        # Callig detect function
-        frame = detect(frame)
-         # Closing the Window   
-        if cv2.waitKey(5) & 0xFF == ord('d'):
-            break
+        isTrue, frame =  video.read()
+        if isTrue == True:
+            # Resizing Frames for better performance
+            frame = imutils.resize(frame , width=min(600,frame.shape[1]))
+            # Calling detect function
+            frame = detect(frame)
+            #writing video with bounding box
+            result.write(frame)
 
-     # Removing capture variable from memory       
+            cv2.imshow('output', frame)
+            # Closing the Window   
+            if cv2.waitKey(10) & 0xFF == ord('d'):
+                break
+
+     # Removing capture variable from memory  
+    result.release()     
     video.release()
     cv2.destroyAllWindows()
 
